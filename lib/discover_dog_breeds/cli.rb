f041f8d6947981ed75475279e4c59cd3a39a9df5
robert-laws <roblaws@me.com>
2017-09-23 11:19:04 +0300

    verify classes can communicate with each other

--- lib/discover_dog_breeds/cli.rb
+++ lib/discover_dog_breeds/cli.rb
@@ -3,4 +3,6 @@ class DiscoverDogBreeds::CLI
   def call
     puts "hello there"
   end
+
+  DiscoverDogBreeds::Scrape.new.say_hello
 end
--- lib/discover_dog_breeds/scrape.rb
+++ lib/discover_dog_breeds/scrape.rb
@@ -1,3 +1,5 @@
 class DiscoverDogBreeds::Scrape
-
+  def say_hello
+    puts "scrape says hello"
+  end
 end
