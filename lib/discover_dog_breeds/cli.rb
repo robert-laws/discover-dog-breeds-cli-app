bb1b0009c547243ba354a8f6e1e1fe8aea5a1f78
robert-laws <roblaws@me.com>
2017-09-23 11:39:20 +0300

    implement CLI#choose_letter method - user must input a letter to advance app

--- lib/discover_dog_breeds/cli.rb
+++ lib/discover_dog_breeds/cli.rb
@@ -1,11 +1,33 @@
 class DiscoverDogBreeds::CLI
+  attr_accessor :count
+
+  def initialize
+    @count = 0
+  end
 
   def call
-    puts "Welcome to the discover dog breeds app. With this tool you can view names of dog breeds and read more information about a selected breed."
+    puts "*** Welcome to the discover dog breeds app. With this tool you can view names of dog breeds and read more information about a selected breed. ***"
     start
   end
 
   def start
+    puts ""
+    p choose_letter
 
+
+  end
+
+  def choose_letter
+    letter = nil
+    if @count > 0
+      puts "You did not select a letter..."
+    end
+
+    puts "Select a letter between A to Z for a list of dogs."
+
+    @count += 1
+    input = gets.strip
+    input.match(/^[a-zA-Z]{1}$/i) ? letter = input.upcase : choose_letter
+    @count = 0
   end
 end
